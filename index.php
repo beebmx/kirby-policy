2884c70875476babadea308cca60d1a41f790f3d
Fernando Gutierrez <fernando@beeb.mx>
2024-05-30 11:52:02 -0600

    wip
    Fixed name in Kirby::plugin

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Kirby\Cms\App as Kirby;
 
 @include_once __DIR__.'/vendor/autoload.php';
 
-Kirby::plugin('beebmx/policy', [
+Kirby::plugin('beebmx/kirby-policy', [
     'options' => [
         'excluded' => 'users',
         'suffix' => 'policy',
